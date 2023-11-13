16be0f2f6f9e895350f0bdf7fbcd345c000aad7c
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-11-13 10:17:43 +0430

    update

--- routes.js
+++ routes.js
@@ -41,7 +41,7 @@ async function auth(req, res, next) {
     const authorization = req.headers.authorization ?? "";
     const token = authorization.split(" ")[1];
 
-    if(!token && req.params.siteId === 'demo') {
+    if(req.params.siteId === 'demo') {
       req.user = {
         id: "demo",
         name: "Demo",
